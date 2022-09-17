adea4f449c6ae9cf2eb5d7ed9fb9ff827cd5e5d9
mldev <marcio449@hotmail.com>
2022-09-17 11:03:53 -0300

    Criação da função dayRate.

---
+++ javascript/freelancer_rates.js
@@ -0,0 +1,20 @@
+ // Freelancer Rates
+
+/*
+Instruções 
+
+Neste exercício, você escreverá código para ajudar um freelancer a se comunicar com seus 
+clientes sobre os preços de determinados projetos. Você escreverá algumas funções utilitárias
+para calcular rapidamente os custos para os clientes.
+
+1. Calcular a taxa diária dada uma taxa horária
+   
+Um cliente entra em contato com o freelancer para saber sobre suas taxas. O freelancer 
+explica que trabalha 8 horas por dia. No entanto, o freelancer sabe apenas suas taxas 
+horárias para o projeto. Ajude-os a estimar uma taxa diária dada uma taxa horária.
+
+*/
+
+function dayRate(hourRate){
+    return hourRate * 8;
+}
