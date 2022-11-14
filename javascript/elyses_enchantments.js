496a1fcaf6ba1809c6fa9aa8f0b7ece326d327ce
mldev <marcio449@hotmail.com>
2022-11-14 20:24:56 -0300

    Inclusão dos comentários de instruções e item 1 recuperar um cartão da pilha

---
+++ javascript/elyses_enchantments.js
@@ -0,0 +1,17 @@
+/*
+Instruções
+
+Como uma futura maga, Elyse precisa praticar algumas noções básicas. Ela tem uma pilha de 
+cartas que ela quer manipular.
+
+Para tornar as coisas um pouco mais fáceis, ela usa apenas as cartas de 1 a 10 para que sua 
+pilha de cartas possa ser representada por uma matriz de números. A posição de um determinado
+cartão corresponde ao índice na matriz. Isso significa que a posição 0 se refere ao primeiro 
+cartão, a posição 1 ao segundo cartão etc. 
+*/
+
+/*
+1. Recuperar um cartão de uma pilha
+   
+Para pegar um cartão, devolva o cartão na posição de índice da pilha fornecida. 
+*/
