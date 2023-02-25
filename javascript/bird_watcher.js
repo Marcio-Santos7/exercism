6eec52b1e1fbb305586070098048c8e77d1a025a
mldev <marcio449@hotmail.com>
2023-02-25 10:35:05 -0300

    Inserção dos comentários de instruções.

---
+++ javascript/bird_watcher.js
@@ -0,0 +1,16 @@
+/*
+Instruções
+
+Você é um ávido observador de pássaros que acompanha quantos pássaros visitaram seu jardim. 
+Normalmente, você usa uma contagem em um caderno para contar os pássaros, mas deseja 
+trabalhar melhor com seus dados. Você já digitalizou as contagens de aves por dia das 
+últimas semanas que guardou no caderno. 
+
+Agora você deseja determinar o número total de aves que contou, calcular a contagem de aves 
+para uma semana específica e corrigir um erro de contagem.
+
+Observação
+
+Para praticar, use um loop for para resolver cada uma das tarefas abaixo. 
+*/
+
