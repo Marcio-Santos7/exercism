c00a62eb5f9e36bfe350fb9402394f96b76a610d
mldev <marcio449@hotmail.com>
2023-03-14 18:35:21 -0300

    Inserção dos comentários do item 1, para a criação da função timeToMixJuice.

--- javascript/mixed_juices.js
+++ javascript/mixed_juices.js
@@ -7,3 +7,18 @@ Sua amiga Li Mei administra uma casa de sucos onde vende deliciosos sucos de fru
 um cliente frequente da loja dela e percebeu que poderia facilitar a vida de sua amiga. Você 
 decide usar suas habilidades de codificação para ajudar Li Mei em seu trabalho.
 */
+
+/*
+1. Determine quanto tempo leva para misturar um suco
+
+Li Mei gosta de avisar seus clientes com antecedência quanto tempo eles terão que esperar 
+por um suco do cardápio que pediram. Ela tem dificuldade em lembrar os números exatos porque 
+o tempo que leva para misturar os sucos varia. 'Pure Strawberry Joy' leva 0,5 minutos, 
+'Energizer' e 'Green Garden' levam 1,5 minutos cada, 'Tropical Island' leva 3 minutos e 
+'All or Nothing' leva 5 minutos. Para todas as outras bebidas (por exemplo, ofertas 
+especiais), você pode considerar um tempo de preparação de 2,5 minutos. 
+
+Para ajudar sua amiga, escreva uma função timeToMixJuice que pega um suco do menu como 
+argumento e retorna o número de minutos que leva para misturar aquela bebida.
+*/
+
