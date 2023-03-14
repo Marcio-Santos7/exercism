b0e54a82f8390846a0016c5950842e6b06171f42
mldev <marcio449@hotmail.com>
2023-03-14 18:31:55 -0300

    Inserção dos comentários de instruções para a criação do programa.

---
+++ javascript/mixed_juices.js
@@ -0,0 +1,9 @@
+// Mixed Juices
+
+/*
+Instruções
+
+Sua amiga Li Mei administra uma casa de sucos onde vende deliciosos sucos de frutas. Você é 
+um cliente frequente da loja dela e percebeu que poderia facilitar a vida de sua amiga. Você 
+decide usar suas habilidades de codificação para ajudar Li Mei em seu trabalho.
+*/
